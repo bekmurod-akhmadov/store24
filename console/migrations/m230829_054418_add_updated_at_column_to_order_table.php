5477473a75aebcc0dd60a3ddba0392ad30a7de14
Tavlonbek Jumayev <tavlonbekjumayev3@gmail.com>
2023-08-29 10:47:27 +0500

    updated at columni orderga qo'shildi

---
+++ console/migrations/m230829_054418_add_updated_at_column_to_order_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles adding columns to table `{{%order}}`.
+ */
+class m230829_054418_add_updated_at_column_to_order_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->addColumn('{{%order}}', 'updated_at', $this->dateTime());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropColumn('{{%order}}', 'updated_at');
+    }
+}
